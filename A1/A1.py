c2ebf286ccf717e0b59f0529a149b8dc4b1f6f90
pascalamiet <61436958+pascalamiet@users.noreply.github.com>
2020-09-15 15:43:00 +0200

    Update A1.py

--- A1/A1.py
+++ A1/A1.py
@@ -1,32 +1,47 @@
 # Assignment 1
 
 # A simple calculator
+print('')
+print('*************************************************************')
+print('')
+print('                  [ THE SIMPLE CALCULATOR ]                  ')
+print('')
 print('INSTRUCTIONS:')
-print('This is a simple calculator')
-print('Choose between add/subtract/multiply/divide')
+print('Choose between [add/subtract/multiply/divide]')
 print('If you want to exit the calculator, type "end"')
+print('')
+print('*************************************************************')
+print('')
+
+# create a list
+l = []
+
+i=0
 
 while True:
-    operator = input('Please enter the operation to be executed [add/subtract/multiply/divide]: ')
+    operator = input('Please enter operator [add/subtract/multiply/divide]: ')
 
     if (operator == 'add'):
         num1 = input('Enter the first number: ')
         num2 = input('Enter the second number: ')
-        print(int(num1) + int(num2))
+        result = int(num1) + int(num2)
+        print(result)
     elif (operator == 'subtract'):
         num1 = input('Enter the first number: ')
         num2 = input('Enter the second number: ')
-        print(int(num1) - int(num2))
+        result = int(num1) - int(num2)
+        print(result)
     elif (operator == 'multiply'):
         num1 = input('Enter the first number: ')
         num2 = input('Enter the second number: ')
-        print(int(num1) * int(num2))
+        result = int(num1) * int(num2)
+        print(result)
     elif (operator == 'divide'):
         num1 = input('Enter the first number: ')
         num2 = input('Enter the second number: ')
-        print(int(num1) / int(num2))
+        result = int(num1) / int(num2)
+        print(result)
     elif (operator == 'end'):
         break
     else:
-        print('Please enter a valid statement')
-
+        print('Please enter a valid statement')  
