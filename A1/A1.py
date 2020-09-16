40f58f240cc00993669814f4458648a8c73ee79c
pascalamiet <61436958+pascalamiet@users.noreply.github.com>
2020-09-16 21:41:52 +0200

    Update A1.py

--- A1/A1.py
+++ A1/A1.py
@@ -4,20 +4,21 @@ import time
 
 # A simple calculator
 print('')
-print('*************************************************************')
+print('**************************************************************')
 print('')
-print('                  [ THE SIMPLE CALCULATOR ]                  ')
+print('                  [ THE SIMPLE CALCULATOR ]                   ')
 print('')
 print('INSTRUCTIONS:')
 print('Choose between [add/subtract/multiply/divide]')
 print('If you want to exit the calculator, type "end"')
 print('To view previous calculations, type "history"')
 print('')
-print('*************************************************************')
+print('**************************************************************')
 print('')
 
 # create a list
 l = []
+m = []
 
 # start counting
 i = 1
@@ -29,33 +30,33 @@ while True:
     if (operator == 'add'):
         num1 = input('Enter the first number: ')
         num2 = input('Enter the second number: ')
-        result = f"{int(num1)} + {int(num2)} = {int(num1) + int(num2)}"
+        result = f"{float(num1)} + {float(num2)} = {float(num1) + float(num2)}"
         print(result)
-        res = int(num1) + int(num2)
+        res = float(num1) + float(num2)
         hist = f"[{i}] {result}"
     elif (operator == 'subtract'):
         num1 = input('Enter the first number: ')
         num2 = input('Enter the second number: ')
-        result = f"{int(num1)} - {int(num2)} = {int(num1) - int(num2)}"
+        result = f"{float(num1)} - {float(num2)} = {float(num1) - float(num2)}"
         print(result)
-        res = int(num1) - int(num2)
+        res = float(num1) - float(num2)
         hist = f"[{i}] {result}"
     elif (operator == 'multiply'):
         num1 = input('Enter the first number: ')
         num2 = input('Enter the second number: ')
-        result = f"{int(num1)} * {int(num2)} = {int(num1) * int(num2)}"
+        result = f"{float(num1)} * {float(num2)} = {float(num1) * float(num2)}"
         print(result)
-        res = int(num1) * int(num2)
+        res = float(num1) * float(num2)
         hist = f"[{i}] {result}"
     elif (operator == 'divide'):
         num1 = input('Enter the first number: ')
         num2 = input('Enter the second number: ')
-        if (int(num2) == 0):
+        if (float(num2) == 0):
             print('ERROR: Division by zero')
             continue
-        result = f"{int(num1)} / {int(num2)} = {int(num1) / int(num2)}"
+        result = f"{float(num1)} / {float(num2)} = {float(num1) / float(num2)}"
         print(result)
-        res = int(num1) / int(num2)
+        res = float(num1) / float(num2)
         hist = f"[{i}] {result}"
     elif (operator == 'end'):
         print('*')
@@ -69,11 +70,9 @@ while True:
         break
     elif (operator == 'history'):
         print(*l, sep = '\n')
-        hist = f"[{i}] {operator}"
-    elif (operator == 'M'):
-        print(*m, sep = '\n')
+        continue
     else:
-        print('Please enter a valid statement')
-        hist = f"[{i}] invalid"
-    i = i + 1
+        print('ERROR: Please enter a valid statement')
+        continue
     l.append(hist)
+    i = i + 1
