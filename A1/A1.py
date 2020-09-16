b14acae1e2cc40da839acfcff65495d7c39876c2
pascalamiet <61436958+pascalamiet@users.noreply.github.com>
2020-09-16 12:09:55 +0200

    Update A1.py

--- A1/A1.py
+++ A1/A1.py
@@ -1,5 +1,7 @@
 # Assignment 1
 
+import time
+
 # A simple calculator
 print('')
 print('*************************************************************')
@@ -9,6 +11,7 @@ print('')
 print('INSTRUCTIONS:')
 print('Choose between [add/subtract/multiply/divide]')
 print('If you want to exit the calculator, type "end"')
+print('To view previous calculations, type "history"')
 print('')
 print('*************************************************************')
 print('')
@@ -19,40 +22,64 @@ l = []
 # start counting
 i = 1
 
+# recall
+m = []
+
+# calculations
 while True:
     operator = input('Please enter operator [add/subtract/multiply/divide]: ')
 
     if (operator == 'add'):
         num1 = input('Enter the first number: ')
         num2 = input('Enter the second number: ')
-        result = int(num1) + int(num2)
+        result = f"{int(num1)} + {int(num2)} = {int(num1) + int(num2)}"
         print(result)
-        hist = f"({i}) {operator} / {num1} / {num2} -> {result}"
+        res = int(num1) + int(num2)
+        hist = f"[{i}] {result}"
     elif (operator == 'subtract'):
         num1 = input('Enter the first number: ')
         num2 = input('Enter the second number: ')
-        result = int(num1) - int(num2)
+        result = f"{int(num1)} - {int(num2)} = {int(num1) - int(num2)}"
         print(result)
-        hist = f"({i}) {operator} / {num1} / {num2} -> {result}"
+        res = int(num1) - int(num2)
+        hist = f"[{i}] {result}"
     elif (operator == 'multiply'):
         num1 = input('Enter the first number: ')
         num2 = input('Enter the second number: ')
-        result = int(num1) * int(num2)
+        result = f"{int(num1)} * {int(num2)} = {int(num1) * int(num2)}"
         print(result)
-        hist = f"({i}) {operator} / {num1} / {num2} -> {result}"
+        res = int(num1) * int(num2)
+        hist = f"[{i}] {result}"
     elif (operator == 'divide'):
         num1 = input('Enter the first number: ')
         num2 = input('Enter the second number: ')
-        result = int(num1) / int(num2)
+        if (int(num2) == 0):
+            print('ERROR: Division by zero')
+            continue
+        result = f"{int(num1)} / {int(num2)} = {int(num1) / int(num2)}"
         print(result)
-        hist = f"({i}) {operator} / {num1} / {num2} -> {result}"
+        res = int(num1) / int(num2)
+        hist = f"[{i}] {result}"
     elif (operator == 'end'):
+        print('*')
+        time.sleep(0.5)
+        print('*')
+        time.sleep(0.5)
+        print('*')
+        time.sleep(0.5)
+        print('Program has finished')
+        print('')
         break
     elif (operator == 'history'):
         print(*l, sep = '\n')
-        hist = f"({i}) {operator}"
+        hist = f"[{i}] {operator}"
+    elif (operator == 'M'):
+        print(*m, sep = '\n')
     else:
         print('Please enter a valid statement')
-        hist = f"({i}) invalid"
+        hist = f"[{i}] invalid"
     i = i + 1
     l.append(hist)
+    m.append(res)
+
+Mi = int(float(m[i-1]))
